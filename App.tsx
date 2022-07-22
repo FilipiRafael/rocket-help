87ca65f960b491b15e12a606f65674f37d5fded2
Filipi Rafael <filipirafael.123@gmail.com>
2022-07-21 21:52:36 -0300

    Add details screen

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { THEME } from './src/styles/theme';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { Loading } from './src/components/Loading';
 import { Home } from './src/screens/Home';
-import { Register } from './src/screens/Register';
+import { Details } from './src/screens/Details';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -12,7 +12,7 @@ export default function App() {
   return (
     <NativeBaseProvider theme={THEME}>
       <StatusBar barStyle='light-content' backgroundColor="transparent" translucent />
-      {fontsLoaded ? <Register /> : <Loading />}
+      {fontsLoaded ? <Details /> : <Loading />}
     </NativeBaseProvider>
   );
 }
---
+++ src/screens/Details.tsx
@@ -0,0 +1,10 @@
+import { VStack } from 'native-base';
+import { Header } from '../components/Header';
+
+export function Details() {
+  return (
+    <VStack flex={1} bg="gray.700">
+        <Header title="Solicitação" />
+    </VStack>
+  );
+}
